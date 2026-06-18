6b8e47f136ba54d2fcffe0202ee988adb9228990
expol1 <13091796r@gmail.com>
2026-06-18 09:07:09 +0200

    dwd

--- assets/providers/apiProvider.js
+++ assets/providers/apiProvider.js
@@ -1,28 +1,38 @@
+let config = {};
+
 export const apiProvider = {
 
+    setConfig(settings) {
+        config = settings || {};
+    },
+
+    getConfig() {
+        return config;
+    },
+
     async testConnection() {
-        console.log("API Test");
+        console.log("API Test", config);
     },
 
     async syncLocations() {
-        console.log("Sync Locations");
+        console.log("Sync Locations", config);
     },
 
     async syncVehicleGroups() {
-        console.log("Sync Vehicle Groups");
+        console.log("Sync Vehicle Groups", config);
     },
 
     async search(filters) {
-        console.log("Search", filters);
+        console.log("Search", filters, config);
         return [];
     },
 
     async createBooking(data) {
-        console.log("Create Booking", data);
+        console.log("Create Booking", data, config);
     },
 
     async cancelBooking(id) {
-        console.log("Cancel Booking", id);
+        console.log("Cancel Booking", id, config);
     }
 
 };
